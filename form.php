cad92440e285e9bdfd1c0a18ddc63340dd2d3306
EDWIN ALEXANDER QUINTERO VELASQUEZ <edwin.quintero@unah.hn>
2021-05-03 14:09:02 -0600

    pruebassss

--- form.php
+++ form.php
@@ -1,11 +1,11 @@
 <?php
  switch($_GET["accion"]){
   case 'SetData':
-    include("../Class/class-form.php");
+    include("class-form.php");
     echo Form::SetForm($_POST);
   break;
   case 'GetData':
-    include("../Class/class-form.php");
+    include("class-form.php");
     echo Form::GetForm();
   break;
  };
